5b52e20d3d6bb2723298e8799f2e8f4b0b5417c7
MyeishaT <trimblem852@gmail.com>
2022-09-15 14:48:14 -0700

    myes section

---
+++ front-page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Template Name: Homepage
+*
+* @package Karot-website
+*
+*/
+?>
+<?php get_header(); ?>
+
+
+<?php get_template_part('template-parts/body')?> 
+
+
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -16,8 +16,12 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<?php wp_head(); ?>
+	
 </head>
 
 <body <?php body_class(); ?>>
@@ -29,20 +33,9 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
+
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$karot_website_description = get_bloginfo( 'description', 'display' );
-			if ( $karot_website_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $karot_website_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+				
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
